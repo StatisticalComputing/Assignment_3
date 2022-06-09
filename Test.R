89b3e485a97b497726c6b8e85c90a0eed3008de6
snehaaaa09 <sneha.ajay9@gmail.com>
2022-06-09 23:57:00 +0200

    updated formula used to calculate no. of models

--- Test.R
+++ Test.R
@@ -10,10 +10,13 @@ print(matrix2008)
 
 ### - Exercise 1 [1 point] - ###
 
-# Since the best linear model can only be allowed to contain at most four predictors and there are 20 possible
-# predictors in total. Hence, (20!/19!) + (20!/18!) + (20!/17!) + (20!/16!).
-answer <- (factorial(20)/factorial(19)) + (factorial(20)/factorial(18)) +
-  (factorial(20)/factorial(17)) + (factorial(20)/factorial(16))
+# Since the best linear model can only be allowed to contain at most
+#four predictors and there are 20 possible predictors in total.
+# Hence, (20!/(19!*1!)) + (20!/(18!*2!)) + (20!/(17!*3!)) + (20!/(16!*4!)).
+answer <- (factorial(20) / (factorial(19) * factorial(1))) +
+(factorial(20) / (factorial(18) * factorial(2))) +
+(factorial(20) / (factorial(17) * factorial(3))) +
+(factorial(20) / (factorial(16) * factorial(4)))
 print(answer)
 
 ### - Exercise 2 [10 point] - ###
